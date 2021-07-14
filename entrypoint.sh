c439e3f38f0bb6dec0dba2cfda919b2408d124f3
Senthil ACS <senthil.acs@gmail.com>
2021-07-14 14:18:05 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -227,8 +227,10 @@ run_nvidia_installer() {
   
   pushd "${NVIDIA_INSTALL_DIR_CONTAINER}"
   TMPDIR=./ \
+  sh NVIDIA-Linux-x86_64-460.73.01.run -x
+  pushd NVIDIA-Linux-x86_64-460.73.01
   IGNORE_MISSING_MODULE_SYMVERS=1 \
-  sh NVIDIA-Linux-x86_64-460.73.01.run \
+  sh nvidia-installer \
     --kernel-source-path="${KERNEL_SRC_DIR}" \
     --no-drm \
     --no-opengl-files \
